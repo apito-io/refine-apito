963edc2bfe6609ba7a3e3a6507ffa1a4987a3544
fahim <fahim.shoumik@gmail.com>
2025-04-10 04:31:19 +0600

    dep upgraded and security issue fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,34 @@
 module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2024: true,
+  },
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:prettier/recommended',
   ],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }; 
